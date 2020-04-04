d274672cee54d740ce5ea89ab29faca14289a86e
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-04 13:32:48 -0500

    page detail using arguments

---
+++ lib/src/pages/movie_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:movies/src/models/movie_model.dart';
+
+class MovieDetailPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final Movie movie = ModalRoute.of(context).settings.arguments;
+
+    return Container(
+      child: Scaffold(
+        body: Center(child: Text(movie.title)),
+      ),
+    );
+  }
+}
--- lib/src/widgets/card_carousel_widget.dart
+++ lib/src/widgets/card_carousel_widget.dart
@@ -33,7 +33,7 @@ class CardCarousel extends StatelessWidget {
   }
 
   Widget _card(BuildContext context, Movie movie) {
-    return Container(
+    final card = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: <Widget>[
@@ -55,32 +55,12 @@ class CardCarousel extends StatelessWidget {
         ],
       )
     );
-  }
 
-  List<Widget> _cards(BuildContext context) {
-    return movies.map((movie) {
-      return Container(
-        margin: EdgeInsets.only(right: 15.0),
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                image: NetworkImage(movie.getPosterImg()),
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                fit: BoxFit.cover,
-                height: 160.0,
-              ),
-            ),
-            SizedBox(height: 5.0),
-            Text(
-              movie.title, 
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            )
-          ],
-        )
-      );
-    }).toList();
+    return GestureDetector(
+      child: card,
+      onTap: () {
+        Navigator.pushNamed(context, 'detail', arguments: movie);
+      },
+    );
   }
 }
