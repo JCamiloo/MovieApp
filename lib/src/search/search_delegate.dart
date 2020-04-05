8e0fca75708d90bc5222194451cd7d73c063aa1d
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-04 20:10:36 -0500

    adding hero animations

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {},
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+
+}
